88417af020237d19fbef950bb29bebf3cb66c538
eeejnlo <jonathan.lee@ericsson.com>
2022-01-05 12:10:53 +0000

    Migrated from tslint to eslint, moved passport config

--- src/app.ts
+++ src/app.ts
@@ -4,53 +4,19 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 import dotenv from "dotenv";
 import cors from "cors";
-import csrf from "csurf";
 import helmet from "helmet";
-import { connect, HydratedDocument } from "mongoose";
-import { UsersRouter } from "./routes/users";
-
-import passport from "passport";
-import { Strategy as LocalStrategy } from "passport-local";
-import { User, UserModel } from "./models/User";
-import bcrypt from "bcrypt";
-
+import { connect } from "mongoose";
 import express_session from "express-session";
-import { NextFunction, Request, Response } from "express-serve-static-core";
+import passport from "passport";
+
+import { UsersRouter } from "./routes/users";
+import { JobsRouter } from "./routes/jobs";
+import { JobMetricsRouter } from "./routes/jobMetrics";
+
+import { passportConfig } from "./config/Passport";
+import { isLoggedIn } from "./config/Auth";
 
 dotenv.config();
-
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const foundUser: HydratedDocument<User> = await UserModel.findOne({
-        email: username,
-      });
-
-      if (!foundUser) {
-        return done(null, false, { message: "Invalid username" });
-      }
-
-      const validPassword = await bcrypt.compare(password, foundUser.password);
-      if (!validPassword) {
-        return done(null, false, { message: "Invalid password" });
-      }
-
-      return done(null, foundUser);
-    } catch (err) {
-      if (err) return done(err);
-    }
-  })
-);
-
-passport.serializeUser((user: HydratedDocument<User>, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-  UserModel.findById(id, (err: any, user: User) => {
-    done(err, user);
-  });
-});
 
 const app = express();
 app.use(
@@ -60,28 +26,30 @@ app.use(
     saveUninitialized: false,
   })
 );
+passportConfig();
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(helmet.hidePoweredBy());
 app.use(logger("dev"));
 app.use(
   cors({
-    origin: "http://localhost:4200",
+    origin: process.env.FRONT_END_URL,
   })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(
-  csrf({
-    cookie: { httpOnly: true },
-    ignoreMethods: ["GET"],
-  })
-);
-app.use((req, res, next) => {
-  res.cookie("XSRF-TOKEN", req.csrfToken());
-  next();
-});
+// TODO re-enable CSRF, (disabled for development)
+// app.use(
+//   csrf({
+//     cookie: { httpOnly: true },
+//     ignoreMethods: ["GET"],
+//   })
+// );
+// app.use((req, res, next) => {
+//   res.cookie("XSRF-TOKEN", req.csrfToken());
+//   next();
+// });
 
 connect("mongodb://localhost:27017/test")
   .then((_) => {
@@ -90,15 +58,6 @@ connect("mongodb://localhost:27017/test")
   .catch((err) => {
     console.error(err);
   });
-
-const isLoggedIn = (req: Request, res: Response, next: NextFunction) => {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  return res
-    .status(401)
-    .json({ message: "You must be logged in to view this resource" });
-};
 
 // TODO remove (used for debugging)
 app.get("/", (req, res) => {
@@ -110,7 +69,9 @@ app.post("/", isLoggedIn, (req, res) => {
   return res.status(200).json({ user: req.user });
 });
 
-app.use("/api/users", UsersRouter);
+app.use("/users", UsersRouter);
+app.use("/jobs", JobsRouter);
+app.use("/metrics/job", JobMetricsRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
