64336d35eb69ae3418c2f5c636307a41d1bd669d
eeejnlo <jonathan.lee@ericsson.com>
2022-01-27 23:36:55 +0000

    Minor refactor

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -3,18 +3,17 @@
 /**
  * Module dependencies.
  */
-import {app} from "../app";
+import {app} from '../app';
+import http from 'http';
+import debug from 'debug';
 
-import http from "http";
+debug('software-delivery-dashboard:server');
 
-import debug from "debug";
-
-debug("express-sandbox:server");
 /**
  * Get port from environment and store in Express.
  */
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 /**
  * Create HTTP server.
@@ -24,64 +23,65 @@ const server = http.createServer(app);
 /**
  * Listen on provided port, on all network interfaces.
  */
-
 server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
+server.on('error', onError);
+server.on('listening', onListening);
 
 /**
  * Normalize a port into a number, string, or false.
+ * @param {Object} val value to be normalized
+ * @return {Object|boolean} calculated port number or boolean
  */
-
 function normalizePort(val: any) {
-    const thisPort = parseInt(val, 10);
+  const thisPort = parseInt(val, 10);
 
-    if (isNaN(thisPort)) {
-        // named pipe
-        return val;
-    }
+  if (isNaN(thisPort)) {
+    // named pipe
+    return val;
+  }
 
-    if (thisPort >= 0) {
-        // port number
-        return thisPort;
-    }
+  if (thisPort >= 0) {
+    // port number
+    return thisPort;
+  }
 
-    return false;
+  return false;
 }
 
 /**
  * Event listener for HTTP server "error" event.
+ * @param {Object} error error to be handled
  */
-
 function onError(error: any) {
-    if (error.syscall !== "listen") {
-        throw error;
-    }
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
 
-    const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case "EACCES":
-            console.error(bind + " requires elevated privileges");
-            process.exit(1);
-            break;
-        case "EADDRINUSE":
-            console.error(bind + " is already in use");
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 }
 
 /**
  * Event listener for HTTP server "listening" event.
  */
-
 function onListening() {
-    const address = server.address();
-    const bind =
-        typeof address === "string" ? "pipe " + address : "port " + address.port;
-    debug("Listening on " + bind);
+  const address = server.address();
+  const bind =
+        typeof address === 'string' ?
+            'pipe ' + address :
+            'port ' + address.port;
+  debug('Listening on ' + bind);
 }
--- src/dao/JenkinsDao.ts
+++ src/dao/JenkinsDao.ts
@@ -1,142 +1,144 @@
-import request from "request";
-import { JobDto } from "../dto/JobDto";
-import { BuildWithDetailsDto } from "../dto/BuildWithDetailsDto";
+import request from 'request';
+import {JobDto} from '../dto/JobDto';
+import {BuildWithDetailsDto} from '../dto/BuildWithDetailsDto';
 
 /**
  * Jenkins data-access-object used to retrieve data from the Jenkins server via the REST API.
  */
 class JenkinsDao {
   /**
-   * @private URL for the Jenkins server which is to be connected to.
-   */
+     * @private URL for the Jenkins server which is to be connected to.
+     */
   private readonly jenkinsServerUrl: string;
 
   /**
-   * @private auth string used for basic authentication when connecting to the Jenkins server.
-   */
+     * @private auth string used for basic authentication when connecting to the Jenkins server.
+     */
   private readonly auth: string;
 
   /**
-   * @param jenkinsServerUrl URL for the Jenkins server which is to be connected to
-   * @param jenkinsServerUsername username to be used when authenticating with the Jenkins server
-   * @param jenkinsServerPassword password to be used when authenticating with the Jenkins server
-   */
+     * @param jenkinsServerUrl URL for the Jenkins server which is to be connected to
+     * @param jenkinsServerUsername username to be used when authenticating with the Jenkins server
+     * @param jenkinsServerPassword password to be used when authenticating with the Jenkins server
+     */
   public constructor(
-    jenkinsServerUrl: string,
-    jenkinsServerUsername: string,
-    jenkinsServerPassword: string
+      jenkinsServerUrl: string,
+      jenkinsServerUsername: string,
+      jenkinsServerPassword: string,
   ) {
     this.jenkinsServerUrl = jenkinsServerUrl;
     this.auth =
-      "Basic " +
-      Buffer.from(`${jenkinsServerUsername}:${jenkinsServerPassword}`).toString(
-        "base64"
-      );
+            'Basic ' +
+            Buffer.from(`${jenkinsServerUsername}:${jenkinsServerPassword}`).toString(
+                'base64',
+            );
   }
 
   /**
-   * Returns all available jobs.
-   *
-   * @returns all available jobs provided in a data transfer object
-   */
+     * Returns all available jobs.
+     *
+     * @return all available jobs provided in a data transfer object
+     */
   public async getAllJobs(): Promise<JobDto[]> {
     return new Promise((resolve, reject) => {
       request(
-        `${this.jenkinsServerUrl}/api/json`,
-        {
-          json: true,
-          headers: { Authorization: this.auth },
-        },
-        (error, response, body) => {
-          if (error) return console.error(error);
-          console.log(
-            `Retrieved all Jenkins jobs with status: ${response.statusCode}`
-          );
-          if (JenkinsDao.isHtmlResponse(body)) {
-            reject(
-              "Jenkins server returned an HTML response indicating no OK HTTP status response"
+          `${this.jenkinsServerUrl}/api/json`,
+          {
+            json: true,
+            headers: {Authorization: this.auth},
+          },
+          (error, response, body) => {
+            if (error) return console.error(error);
+            console.log(
+                `Retrieved all Jenkins jobs with status: 
+                ${response.statusCode}`,
             );
-          }
-          resolve(body.jobs);
-        }
+            if (JenkinsDao.isHtmlResponse(body)) {
+              reject(
+                  'Jenkins server returned an HTML response ' +
+                  'indicating no OK HTTP status response',
+              );
+            }
+            resolve(body.jobs);
+          },
       );
     });
   }
 
   /**
-   * Returns the job specified by the provided name.
-   *
-   * @param jobName name of the job to be returned
-   * @returns job information provided in a data transfer object
-   */
+     * Returns the job specified by the provided name.
+     *
+     * @param jobName name of the job to be returned
+     * @return job information provided in a data transfer object
+     */
   public async getJob(jobName: string): Promise<JobDto> {
     return new Promise((resolve, reject) => {
       request(
-        `${this.jenkinsServerUrl}/job/${jobName}/api/json`,
-        {
-          json: true,
-          headers: { Authorization: this.auth },
-        },
-        (error, response, body) => {
-          if (error) return console.error(error);
-          console.log(
-            `Retrieved job [${jobName}] with status: ${response.statusCode}`
-          );
-          if (JenkinsDao.isHtmlResponse(body)) {
-            reject(
-              "Jenkins server returned an HTML response indicating no OK HTTP status response"
+          `${this.jenkinsServerUrl}/job/${jobName}/api/json`,
+          {
+            json: true,
+            headers: {Authorization: this.auth},
+          },
+          (error, response, body) => {
+            if (error) return console.error(error);
+            console.log(
+                `Retrieved job [${jobName}] with status: ${response.statusCode}`,
             );
-          }
-          resolve(body);
-        }
+            if (JenkinsDao.isHtmlResponse(body)) {
+              reject(
+                  'Jenkins server returned an HTML response indicating no OK HTTP status response',
+              );
+            }
+            resolve(body);
+          },
       );
     });
   }
 
   /**
-   * Retrieves the build specified by job name and build number.
-   *
-   * @param jobName name of the job for which data is to be retrieved
-   * @param buildNumber number for the given build
-   * @returns build information provided in a data transfer object
-   */
+     * Retrieves the build specified by job name and build number.
+     *
+     * @param jobName name of the job for which data is to be retrieved
+     * @param buildNumber number for the given build
+     * @return build information provided in a data transfer object
+     */
   public async getBuild(
-    jobName: string,
-    buildNumber: number
+      jobName: string,
+      buildNumber: number,
   ): Promise<BuildWithDetailsDto> {
     return new Promise((resolve, reject) => {
       request(
-        `${this.jenkinsServerUrl}/job/${jobName}/${buildNumber}/api/json`,
-        {
-          json: true,
-          headers: { Authorization: this.auth },
-        },
-        (error, response, body) => {
-          if (error) return console.error(error);
-          console.log(
-            `Retrieved build [${jobName}/${buildNumber}] with status: ${response.statusCode}`
-          );
-          if (JenkinsDao.isHtmlResponse(body)) {
-            reject(
-              "Jenkins server returned an HTML response indicating no OK HTTP status response"
+          `${this.jenkinsServerUrl}/job/${jobName}/${buildNumber}/api/json`,
+          {
+            json: true,
+            headers: {Authorization: this.auth},
+          },
+          (error, response, body) => {
+            if (error) return console.error(error);
+            console.log(
+                `Retrieved build [${jobName}/${buildNumber}] with status: ${response.statusCode}`,
             );
-          }
-          resolve(body);
-        }
+            if (JenkinsDao.isHtmlResponse(body)) {
+              reject(
+                  'Jenkins server returned an HTML response indicating no OK HTTP status response',
+              );
+            }
+            resolve(body);
+          },
       );
     });
   }
 
   /**
-   * Determines whether the provided HTTP response body is an HTML response.
-   *
-   * @param body HTTP response body to be checked
-   * @returns boolean indicating whether the provided HTTP response body is an HTML response
-   * @private used within the Jenkins data access object class only
-   */
+     * Determines whether the provided HTTP response body is an HTML response.
+     *
+     * @param body HTTP response body to be checked
+     * @return boolean indicating whether the provided HTTP response body is an HTML response
+     * @private used within the Jenkins data access object class only
+     */
   private static isHtmlResponse(body: any): boolean {
-    return String(body).startsWith("<!doctype html>");
+    return String(body).startsWith('<!doctype html>');
   }
 }
 
-export { JenkinsDao };
+export {JenkinsDao};
